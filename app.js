2f8f1f88c9efc28ed05570b4b8601a71d4dc0231
devHussainabbas <03555abc@gmail.com>
2024-05-13 21:40:44 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+function jamal(){
+    document.getElementById("para").style.backgroundColor="blue";
+    document.getElementById("para").style.border="3px solid red";
+    document.getElementById("para").style.color="white";
+    document.getElementById("para").style.fontSize="17px";
+    document.getElementById("para").style.borderRadius="10px";
+    
+    
+}
+function jamil(){
+    document.getElementById("para").style.backgroundColor="orangered";
+    document.getElementById("para").style.border="3px solid blue";
+    document.getElementById("para").style.color="blue";
+    
+    
+}
+function jamshed(){
+    document.getElementById("para").style.backgroundColor="green";
+    document.getElementById("para").style.border="3px solid purple";
+    document.getElementById("para").style.color="yellow";
+    document.getElementById("para").style.borderRadius="10px";
+    
+    
+}
+function arbab(){
+    document.getElementById("para").style.backgroundColor="purple";
+    document.getElementById("para").style.border="3px solid orangered";
+    document.getElementById("para").style.color="white";
+    
+    
+}
